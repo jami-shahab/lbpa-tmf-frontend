bb1857653d8ca49401ad52cbf3b56065a7971e99
jami-shahab <shahabjami14@gmail.com>
2025-11-24 16:30:09 -0500

    fixed tailwind dependency issue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/postcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
